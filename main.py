6f4d7447a023b8274319ab42e8a44334137623e3
Gareth Tribello <gareth.tribello@gmail.com>
2020-11-23 15:02:42 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+
+def factorial(n) : 
+  # Your code to calcualte n! using recursion goes here
+  
+  
+# You should not need to adjust anything from here.  The code here
+# is just for testing if your factorial is working
+for i in range(1,10) : print( factorial(i) )
